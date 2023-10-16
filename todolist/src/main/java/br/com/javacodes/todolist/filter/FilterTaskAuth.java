eb676f8c5058dc5ace7cd331365ae96d5dc07b94
Rennan Ventura <rennan.ventura.alves@gmail.com>
2023-10-16 00:37:00 -0300

    Validando o username

--- todolist/src/main/java/br/com/javacodes/todolist/filter/FilterTaskAuth.java
+++ todolist/src/main/java/br/com/javacodes/todolist/filter/FilterTaskAuth.java
@@ -3,10 +3,12 @@ package br.com.javacodes.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.javacodes.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 
@@ -15,6 +17,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter{
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -33,16 +38,24 @@ public class FilterTaskAuth extends OncePerRequestFilter{
                 String[] credentials = authString.split(":");
                 String username = credentials[0];
                 String password = credentials[1];
-                System.out.println("aqui");
-                System.out.println(username);
-                System.out.println(password);
 
 
-                
                 //Validar o usuario
-                //Validar a senha
-                //continua
-                filterChain.doFilter(request, response);
+                var user = this.userRepository.findByUsername(username);
+                if(user == null){
+                    response.sendError(401);
+                } else {
+                    //Validar a senha
+                    var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                    if(passwordVerify.verified){
+                        filterChain.doFilter(request, response);
+                    } else {
+                        response.sendError(401);
+                    }
+                    //continua
+                    
+                }
+
     }
 
     

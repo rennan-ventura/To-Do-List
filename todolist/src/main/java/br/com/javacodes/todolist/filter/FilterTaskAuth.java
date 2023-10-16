079d393adb2885e3b096fff0a78ef1bd404ae17c
Rennan Ventura <rennan.ventura.alves@gmail.com>
2023-10-16 00:30:16 -0300

    Autenticando o usuario

---
+++ todolist/src/main/java/br/com/javacodes/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,50 @@
+package br.com.javacodes.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter{
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+                //Pegar a autenticação (usuario e senha)
+                var authorization = request.getHeader("Authorization");
+                
+                var authEncoded = authorization.substring("Basic".length()).trim();
+                
+                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+                var authString = new String(authDecode);
+                
+
+                String[] credentials = authString.split(":");
+                String username = credentials[0];
+                String password = credentials[1];
+                System.out.println("aqui");
+                System.out.println(username);
+                System.out.println(password);
+
+
+                
+                //Validar o usuario
+                //Validar a senha
+                //continua
+                filterChain.doFilter(request, response);
+    }
+
+    
+    
+}
---
+++ todolist/src/main/java/br/com/javacodes/todolist/task/TaskController.java
@@ -0,0 +1,27 @@
+package br.com.javacodes.todolist.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.PostConstruct;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel){
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskModel);
+    }
+    
+}
